c9b9e6578ee930bac183d134b1e1016a7eeccacd
Saher-Amasha <saheramasha0@gmail.com>
2024-03-02 02:39:16 +0200

    fix formating

--- shapes/rectangle.py
+++ shapes/rectangle.py
@@ -3,10 +3,18 @@ from position import Position
 from shapes.shape import Shape
 
 
-class rectangle(Shape):
-    def __init__(self, id, position: Position, color: str,height:int,width:int) -> None:
-        super().__init__(id, position, color)
+class Rectangle(Shape):
+    """
+    Rectangle class
+    """
+    def __init__(self, shape_id, position: Position, color: str,height:int,width:int) -> None:
+        super().__init__(shape_id, position, color)
         self.width=width
         self.height=height
+
     def draw(self,window):
-        return pygame.draw.rect(window,self.color, (self.position.x*8, self.position.y*8, self.width, self.height))
+        """
+        Draws rectangle
+        """
+        return pygame.draw.rect(window,self.color,
+                                (self.position.x*8, self.position.y*8, self.width, self.height))
